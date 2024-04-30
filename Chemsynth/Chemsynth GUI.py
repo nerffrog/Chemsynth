d18b272f9bb75da328bc9c4efd0bb5fe0f5d4d1b
nerffrog <mios2.shaaban@yahoo.com>
2024-05-01 00:03:29 +0200

    Made code a bit more readable

--- Chemsynth/Chemsynth GUI.py
+++ Chemsynth/Chemsynth GUI.py
@@ -202,130 +202,102 @@ def tank_screen():
         current_tool = None
         total_points = total_points + points
         points_message = "Total Score: " + str(total_points)
-        points_label = GUI.Label(root, text = points_message, font = ("Helvetica", 25))
-        points_label.configure(background = "purple")
+        points_label = GUI.Label(root, text = points_message, font = ("Helvetica", 25), background = "purple")
         points_label.place(x = 250, y = 150, height = 200, width = 300)
-
-        play_again_button = GUI.Button(root, text = "Play Again", font = ("Helvetica", 20), command = tank_generator)
-        play_again_button.configure(background = "cyan")
+        play_again_button = GUI.Button(root, text = "Play Again", font = ("Helvetica", 20), command = tank_generator, background = "cyan")
         play_again_button.place(x = 300, y = 350, height = 100, width = 200)
 
     else:
-        stirrer = GUI.Button(root, image = stirrer_image, command = lambda: select_tool("stirrer")) #lambda lets me select function and send it an argument
-        stirrer.configure(background = "purple", borderwidth= 0)
+        stirrer = GUI.Button(root, image = stirrer_image, command = lambda: select_tool("stirrer"), background = "purple", borderwidth= 0) #lambda lets me select function and send it an argument
         stirrer.place(x = 360, y = 350, height = 150, width = 120)
 
-        centrifuge = GUI.Button(root, image = centrifuge_image, command = lambda: select_tool("centrifuge"))
-        centrifuge.configure(background = "purple", borderwidth= 0)
+        centrifuge = GUI.Button(root, image = centrifuge_image, command = lambda: select_tool("centrifuge"), background = "purple", borderwidth= 0)
         centrifuge.place(x = 480, y = 350, height = 150, width = 120)
 
-        replicator = GUI.Button(root, image = replicator_image, command = lambda: select_tool("replicator"))
-        replicator.configure(background = "purple", borderwidth= 0)
+        replicator = GUI.Button(root, image = replicator_image, command = lambda: select_tool("replicator"), background = "purple", borderwidth= 0)
         replicator.place(x = 240, y = 350, height = 150, width = 120)
 
-        solvent = GUI.Button(root, image = solvent_image, command = lambda: select_tool("solvent"))
-        solvent.configure(background = "purple", borderwidth= 0)
+        solvent = GUI.Button(root, image = solvent_image, command = lambda: select_tool("solvent"), background = "purple", borderwidth= 0)
         solvent.place(x = 0, y = 350, height = 150, width = 120)
 
-        catalyst = GUI.Button(root, image = catalyst_image, command = lambda: select_tool("catalyst"))
-        catalyst.configure(background = "purple", borderwidth= 0)
+        catalyst = GUI.Button(root, image = catalyst_image, command = lambda: select_tool("catalyst"), background = "purple", borderwidth= 0)
         catalyst.place(x = 120, y = 350, height = 150, width = 120)
 
         #tanks 1-10 as currently seen
-        tank1 = GUI.Button(root, command = lambda: use_tool(1)) #pos doesn't start from 0 obviously (took some time to make it this way)
-        tank1.configure(background = tanks[0])
+        #pos doesn't start from 0 obviously (took some time to make it this way)
+        tank1 = GUI.Button(root, command = lambda: use_tool(1), background = tanks[0]) 
         tank1.place(x = 0, y = 200, height = 80, width = 80)
 
-        tank2 = GUI.Button(root, command = lambda: use_tool(2))
-        tank2.configure(background = tanks[1])
+        tank2 = GUI.Button(root, command = lambda: use_tool(2), background = tanks[1])
         tank2.place(x = 80, y = 200, height = 80, width = 80)
 
-        tank3 = GUI.Button(root, command = lambda: use_tool(3))
-        tank3.configure(background = tanks[2])
+        tank3 = GUI.Button(root, command = lambda: use_tool(3), background = tanks[2])
         tank3.place(x = 160, y = 200, height = 80, width = 80)
 
-        tank4 = GUI.Button(root, command = lambda: use_tool(4))
-        tank4.configure(background = tanks[3])
+        tank4 = GUI.Button(root, command = lambda: use_tool(4), background = tanks[3])
         tank4.place(x = 240, y = 200, height = 80, width = 80)
 
-        tank5 = GUI.Button(root, command = lambda: use_tool(5))
-        tank5.configure(background = tanks[4])
+        tank5 = GUI.Button(root, command = lambda: use_tool(5), background = tanks[4])
         tank5.place(x = 320, y = 200, height = 80, width = 80)
 
-        tank6 = GUI.Button(root, command = lambda: use_tool(6))
-        tank6.configure(background = tanks[5])
+        tank6 = GUI.Button(root, command = lambda: use_tool(6), background = tanks[5])
         tank6.place(x = 400, y = 200, height = 80, width = 80)
 
-        tank7 = GUI.Button(root, command = lambda: use_tool(7))
-        tank7.configure(background = tanks[6])
+        tank7 = GUI.Button(root, command = lambda: use_tool(7), background = tanks[6])
         tank7.place(x = 480, y = 200, height = 80, width = 80)
 
-        tank8 = GUI.Button(root, command = lambda: use_tool(8))
-        tank8.configure(background = tanks[7])
+        tank8 = GUI.Button(root, command = lambda: use_tool(8), background = tanks[7])
         tank8.place(x = 560, y = 200, height = 80, width = 80)
 
-        tank9 = GUI.Button(root, command = lambda: use_tool(9))
-        tank9.configure(background = tanks[8])
+        tank9 = GUI.Button(root, command = lambda: use_tool(9), background = tanks[8])
         tank9.place(x = 640, y = 200, height = 80, width = 80)
 
-        tank10 = GUI.Button(root, command = lambda: use_tool(10))
-        tank10.configure(background = tanks[9])
+        tank10 = GUI.Button(root, command = lambda: use_tool(10), background = tanks[9])
         tank10.place(x = 720, y = 200, height = 80, width = 80)
 
         #tanks 1-10 as actually required
-        req1 = GUI.Label(root)
-        req1.configure(background = tanks_required[0])
+        req1 = GUI.Label(root, background = tanks_required[0])
         req1.place(x = 25, y = 280, height = 20, width = 30)
 
-        req2 = GUI.Label(root)
-        req2.configure(background = tanks_required[1])
+        req2 = GUI.Label(root, background = tanks_required[1])
         req2.place(x = 105, y = 280, height = 20, width = 30)
 
-        req3 = GUI.Label(root)
-        req3.configure(background = tanks_required[2])
+        req3 = GUI.Label(root, background = tanks_required[2])
         req3.place(x = 185, y = 280, height = 20, width = 30)
 
-        req4 = GUI.Label(root)
-        req4.configure(background = tanks_required[3])
+        req4 = GUI.Label(root, background = tanks_required[3])
         req4.place(x = 265, y = 280, height = 20, width = 30)
 
-        req5 = GUI.Label(root)
-        req5.configure(background = tanks_required[4])
+        req5 = GUI.Label(root, background = tanks_required[4])
         req5.place(x = 345, y = 280, height = 20, width = 30)
 
-        req6 = GUI.Label(root)
-        req6.configure(background = tanks_required[5])
+        req6 = GUI.Label(root, background = tanks_required[5])
         req6.place(x = 425, y = 280, height = 20, width = 30)
 
-        req7 = GUI.Label(root)
-        req7.configure(background = tanks_required[6])
+        req7 = GUI.Label(root, background = tanks_required[6])
         req7.place(x = 505, y = 280, height = 20, width = 30)
 
-        req8 = GUI.Label(root)
-        req8.configure(background = tanks_required[7])
+        req8 = GUI.Label(root, background = tanks_required[7])
         req8.place(x = 585, y = 280, height = 20, width = 30)
 
-        req9 = GUI.Label(root)
-        req9.configure(background = tanks_required[8])
+        req9 = GUI.Label(root, background = tanks_required[8])
         req9.place(x = 665, y = 280, height = 20, width = 30)
 
-        req10 = GUI.Label(root)
-        req10.configure(background = tanks_required[9])
+        req10 = GUI.Label(root, background = tanks_required[9])
         req10.place(x = 745, y = 280, height = 20, width = 30)
 
         #current score
         points_message = "Score: " + str(points)
-        points_label = GUI.Label(root, text = points_message, font= ("Helvetica", 25))
-        points_label.configure(background = "purple")
+        points_label = GUI.Label(root, text = points_message, font= ("Helvetica", 25), background = "purple")
         points_label.place(x = 250, y = 0, height = 150, width = 300)
 
 total_points = 0
+game_number = 1
 root = GUI.Tk() #creates the window/parent
 root.title("Chemsynth")
 root.geometry("800x500") #x by y
 root.resizable(False, False) #one for each dimension
 root.configure(background = "purple") #configure is usable for all widgets
-
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 stirrer_image = GUI.PhotoImage(file = os.path.join(current_dir, "Stirrer.png"))
@@ -335,10 +307,7 @@ solvent_image = GUI.PhotoImage(file = os.path.join(current_dir, "Solvent.png"))
 catalyst_image = GUI.PhotoImage(file = os.path.join(current_dir, "Catalyst.png"))
 rainbow_image = GUI.PhotoImage(file = os.path.join(current_dir, "Rainbow.png"))
 
-
-start_button = GUI.Button(root, text = "Play", command = tank_generator, font = ("Helvetica", 25)) #first argument is parent it belongs to - command is name of function WITHOUT ()
-start_button.configure(background = "cyan")
+start_button = GUI.Button(root, text = "Play", command = tank_generator, font = ("Helvetica", 25), background = "cyan") #first argument is parent it belongs to - command is name of function WITHOUT ()
 start_button.place(x = 300, y = 225, width = 200, height = 50) #also possible to use pack() and grid()
 
-game_number = 1
 root.mainloop() #runs the window
